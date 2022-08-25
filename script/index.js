4054405bdb6b5898fb331649ea7bee7aa518970d
mogoatlhe <iam@ofent.se>
2022-08-25 17:02:25 +0200

    use nodejs to serve pages

--- script/index.js
+++ script/index.js
@@ -1,9 +1,25 @@
 import http from "http";
+import fs from "fs";
 
 const port = 3000;
 
-const server = http.createServer((req, res) => {});
+const htmlFiles = fs.readdirSync("./", { withFileTypes: true });
+
+const server = http.createServer((req, res) => {
+  const url = req.url;
+
+  if (url === "/") {
+    const file = fs.readFileSync("./index.html");
+    res.end(file);
+  } else if (htmlFiles.find((currFile) => url.includes(currFile.name))) {
+    const file = fs.readFileSync(`.${url}`);
+    res.end(file);
+  } else {
+    const file = fs.readFileSync(`./404.html`);
+    res.end(file);
+  }
+});
 
 server.listen(port, () => {
-  console.log(`Server running on port ${port}`);
+  console.log(`Server running on port  ${port}`);
 });
